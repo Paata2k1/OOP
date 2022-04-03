289dd8508c2e8b622db60ba71363c71309809cd9
Paata <kharashvili20@gmail.com>
2022-04-03 14:00:59 +0400

    __init__()

---
+++ Flight.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+#x = ''.join(random.choice(string.ascii_uppercase) for _ in range(5))
+#print(x)
+
+
+class Flight:
+
+    def __init__(self, id, source, destination, duration, seats, passengers):
+        self.id = id
+        self.source = source
+        self.destination = destination
+        self.duration = duration
+        self.seats = seats
+        self.passengers = passengers
+
+print(x)
